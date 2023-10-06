6330da50ecb7ef3c33180bb48c6641c4adeb1b50
sankthomas <thomassankara74@gmail.com>
2023-10-06 06:44:36 +0300

    update pages

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,4 @@
-import {
-  View,
-  Text,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
+import { View, Text, SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import React from "react";
 import { globals } from "../styles/globals";
 import Services from "../components/Services";
@@ -15,25 +8,6 @@ export default function Home({ navigation }) {
     <SafeAreaView style={globals.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.pageContainer}>
-          <Text style={globals.heading}>The Rose Graphics</Text>
-
-          <View>
-            <Text style={[globals.text, styles.text]}>
-              We deal in design and wireframing of company brochures, logos,
-              flyers, business cards, advertisements and more. Anything you need
-              designed, we do it!
-            </Text>
-
-            <TouchableOpacity style={globals.buttonContainer}>
-              <Text
-                style={globals.button}
-                onPress={() => navigation.navigate("Designs")}
-              >
-                All Designs
-              </Text>
-            </TouchableOpacity>
-          </View>
-
           <Services navigation={navigation} />
         </View>
       </ScrollView>
@@ -45,9 +19,5 @@ const styles = StyleSheet.create({
   pageContainer: {
     paddingVertical: 32,
     paddingHorizontal: 16,
-  },
-  text: {
-    marginTop: 10,
-    marginBottom: 20,
   },
 });
--- screens/Service.js
+++ screens/Service.js
@@ -1,10 +1,50 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
+import { globals } from "../styles/globals";
+import Header from "../components/Header";
 
-export default function Service() {
+export default function Service({ navigation, route }) {
   return (
-    <View>
-      <Text>Service</Text>
-    </View>
+    <>
+      <SafeAreaView style={globals.container}>
+        <Header navigation={navigation} route={route} />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={globals.pageContainer}>
+            <Text style={globals.heading}>{route.params.title}</Text>
+
+            <Image style={styles.image} source={{ uri: route.params.image }} />
+
+            <Text style={[globals.text, styles.text]}>{route.params.desc}</Text>
+
+            <TouchableOpacity style={globals.buttonContainer}>
+              <Text style={globals.button}>
+                Contact Me For {route.params.title} Service
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 250,
+    resizeMode: "cover",
+    borderRadius: 16,
+    marginTop: 32,
+  },
+  text: {
+    marginVertical: 32,
+  },
+});
